41936d6c310633868ac6589723bec602bc9351b5
stefan-lazarovski <stefan.lazarovski@protonmail.com>
2026-08-28 00:18:27 +0200

    add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/stefan-lazarovski/idrs',
+    site: 'https://stefan-lazarovski.github.io/idrs',
     i18n: {
         locales: ["mk", "en"],
         defaultLocale: "mk",
