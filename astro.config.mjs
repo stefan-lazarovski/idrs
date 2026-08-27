49498b8f8ea0d41c30b5c5567fe7795bd906c8f8
stefan-lazarovski <stefan.lazarovski@protonmail.com>
2026-08-28 00:39:24 +0200

    hotfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://stefan-lazarovski.github.io',
+    base: '/idrs',
     i18n: {
         locales: ["mk", "en"],
         defaultLocale: "mk",
